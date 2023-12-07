364ea60fd45deb6376580b5df973dbcffc3d3352
uzr <uzr@ASUSPUTER>
2023-12-06 22:29:43 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using PaOaTableProject;
+using PaOaTableProject.Models;
+using System.Data;
+using static PaOaTableProject.Models.RolidexModel;
+using Newtonsoft.Json.Linq;
+
+
+
+namespace PaOaTableProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string jsonStringInput)
+        {
+            string stringInput;
+            List<RolidexModel> items = new List<RolidexModel>();
+            Newtonsoft.Json.Linq.JObject jsonInput;
+            var rolidexDisplay = new RolidexModel();
+            var rolidexCards = new RolidexModel.RolidexCards();
+
+            if (String.IsNullOrWhiteSpace(jsonStringInput))
+            {
+                using (StreamReader sr = new StreamReader("C:\\Code\\DataDir\\employee_data.json"))
+                {
+                    string json = sr.ReadToEnd();
+                    stringInput = json;
+                    var jsonPull = JsonConvert.DeserializeObject(json);
+                    jsonInput = (Newtonsoft.Json.Linq.JObject)jsonPull;
+                    if (jsonPull != null)
+                    {
+                        System.Diagnostics.Debug.WriteLine("not null");
+                    }
+                }
+            }
+            else
+            {
+                jsonInput= JObject.Parse(jsonStringInput);
+            }
+
+
+            var val1 = jsonInput.Children();
+            var val2 = val1.Children();
+            var val3 = val2.Children();
+            rolidexDisplay.RolidexCardList = new List<RolidexCard>();
+
+            foreach (var x in val3)
+            {
+                RolidexCard rc = new RolidexCard();
+                //id
+                rc._id = x.First.ToString();
+                rc._id = x.SelectToken("_id").ToString();
+                //name
+                rc.name = x.SelectToken("name").ToString();
+                //email address
+                rc.email = x.SelectToken("email").ToString();
+                //street address
+                rc.addressStreet = x.SelectToken("address.street").ToString();
+                rc.addressTown = x.SelectToken("address.town").ToString();
+                rc.addressPostcode = x.SelectToken("address.postode").ToString();
+
+                //pets
+                var pets = x.SelectToken("pets").Children();
+                foreach (var y in pets)
+                {
+                    rc.petsAll += y.ToString();
+                    rc.petsAll += ", ";
+                }
+                rc.petsAll = rc.petsAll.Substring(0, rc.petsAll.Length - 2);
+
+                rolidexDisplay.RolidexCardList.Add(rc);
+
+            }
+            ViewBag.Message = "Your application description page.";
+            rolidexCards.RolidexCardList = rolidexDisplay.RolidexCardList;
+            return View(rolidexCards);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/RolidexModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+
+namespace PaOaTableProject.Models
+{
+    public class RolidexModel
+    {
+        public DataTable RolidexDataTable { get; set; }
+        public List<RolidexCard> RolidexCardList { get; set; }
+
+        public class RolidexCards
+        {
+            public List<RolidexCard> RolidexCardList { get; set; }
+        }
+
+        public class Address
+        {
+            public string street { get; set; }
+            public string town { get; set; }
+            public string postode { get; set; }
+        }
+
+        public class RolidexCard
+        {
+            public string _id { get; set; }
+            public string name { get; set; }
+            public string dob { get; set; }
+            public Address address { get; set; }
+            public string addressStreet { get; set; }
+            public string addressPostcode { get; set; }
+            public string addressTown { get; set; }
+            public string telephone { get; set; }
+            public List<string> pets { get; set; }
+            public string petsAll { get; set; }
+            public double score { get; set; }
+            public string email { get; set; }
+            public string url { get; set; }
+            public string description { get; set; }
+            public bool verified { get; set; }
+            public int salary { get; set; }
+        }
+    }
+}
